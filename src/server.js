c1250e181f92cc5f6f8d6c0796a3f8c31ea1937b
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2026-02-05 17:56:20 -0300

    feat: regras methods

--- src/server.js
+++ src/server.js
@@ -9,11 +9,24 @@ import http from 'node:http';
 // PATCH => Atualizar uma informação específica de um recurso no back-end
 // DELETE => Remover um recurso no back-end
 
+
+// GET/users => Buscando usuários no back-end
+// POST/users => Criando um usuário no back-end
+// PUT/users/123 => Atualizando o usuário de ID 123 no back-end
+// DELETE/users/123 => Removendo o usuário de ID 123 no back-end
+
 const server = http.createServer((req, res) => {
     const { method, url } = req;
     console.log(method, url);
-    return res.end(`Method: ${method}, URL: ${url}`);
-    // console.log('Servidor rodando');
+    
+    if (method == 'GET' && url === '/users') {
+        return res.end('Listagem de usuários');
+    }
+
+    if (method == 'POST' && url === '/users') {
+        return res.end('Criação de um usuário');
+    }
+
 });
 
 server.listen(3333);
