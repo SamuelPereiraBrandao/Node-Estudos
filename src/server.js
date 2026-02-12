6a0b00a13fd71461ef2ebbcfc72bc20cc26fdc8d
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2026-02-12 16:27:35 -0300

    feat: crud completo feito

--- src/server.js
+++ src/server.js
@@ -17,10 +17,15 @@ const server = http.createServer(async (req, res) => {
     await json(req, res);
     
     const route = routes.find(route => {
-        return route.method === method && route.path === url;
+        return route.method === method && route.path.test(url);
     })
 
     if (route) {
+        const routeParms = req.url.match(route.path);
+
+        req.params = { ...routeParms.groups }
+
+       
         return route.handler(req, res);
     }
 
